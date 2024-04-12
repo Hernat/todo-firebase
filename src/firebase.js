13b47b2bf0c199d39d717b61876dfb7d791df7c6
Hernat <tokyhernat@gmail.com>
2024-04-12 15:21:07 +0300

    add editorconfig and prettier

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,8 +14,8 @@ const firebaseConfig = {
     messagingSenderId: "469294343905",
     appId: "1:469294343905:web:33817751fa952b85ebf9a0",
     measurementId: "G-6NSRMPE8E2",
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
